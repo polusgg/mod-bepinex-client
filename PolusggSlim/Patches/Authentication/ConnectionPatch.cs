cb47e705a3becba42f7788e3898d3ff2e2ff0b7a
cybershard <ashwatdd01@gmail.com>
2021-05-09 20:04:52 -0400

    Twentieth Commit

--- PolusggSlim/Patches/Authentication/ConnectionPatch.cs
+++ PolusggSlim/Patches/Authentication/ConnectionPatch.cs
@@ -10,19 +10,30 @@ namespace PolusggSlim.Patches.Authentication
         [HarmonyPatch(typeof(UnityUdpClientConnection), nameof(UnityUdpClientConnection.WriteBytesToConnection))]
         public static class UnityUdpClientConnection_WriteBytesToConnection
         {
-            public static void Prefix([HarmonyArgument(0)] ref Il2CppStructArray<byte> bytes,
-                [HarmonyArgument(1)] ref int length)
+            public static void Prefix(
+                [HarmonyArgument(0)] ref Il2CppStructArray<byte> bytes,
+                [HarmonyArgument(1)] ref int length) => SignByteArray(ref bytes, ref length);
+        }
+
+        [HarmonyPatch(typeof(UnityUdpClientConnection), nameof(UnityUdpClientConnection.WriteBytesToConnectionSync))]
+        public static class UnityUdpClientConnection_WriteBytesToConnectionSync
+        {
+            public static void Prefix(
+                [HarmonyArgument(0)] ref Il2CppStructArray<byte> bytes,
+                [HarmonyArgument(1)] ref int length) => SignByteArray(ref bytes, ref length);
+        }
+
+        private static void SignByteArray(ref Il2CppStructArray<byte> bytes, ref int length)
+        {
+            // if (ServerManager.Instance.CurrentRegion.PingServer ==
+            //     PluginSingleton<PolusggMod>.Instance.Configuration.Server.IpAddress)
+            // {
+            if (bytes[0] != 0x09 && AmongUsClient.Instance.GameMode == GameModes.OnlineGame)
             {
-                // if (ServerManager.Instance.CurrentRegion.PingServer ==
-                //     PluginSingleton<PolusggMod>.Instance.Configuration.Server.IpAddress)
-                // {
-                if (bytes[0] != 0x09 && AmongUsClient.Instance.GameMode == GameModes.OnlineGame)
-                {
-                    PluginSingleton<PolusggMod>.Instance.SigningHelper.SignByteArray(ref bytes);
-                    length = bytes.Length;
-                }
-                // }
+                PluginSingleton<PolusggMod>.Instance.SigningHelper.SignByteArray(ref bytes);
+                length = bytes.Length;
             }
+            // }
         }
     }
 }
