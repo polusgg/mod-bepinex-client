7c14b84da90a20086a54fbfac09bb6f15828daf4
Sanae <atmatm6000@outlook.com>
2021-05-19 15:40:19 -0600

    Finally finish adding bitfield to player animator

--- PolusGGMod/Behaviours/PlayerAnimPlayer.cs
+++ PolusGGMod/Behaviours/PlayerAnimPlayer.cs
@@ -38,34 +38,39 @@ namespace PolusGG.Behaviours {
         public void HandleMessage(MessageReader reader) {
             //todo handle player messages reading like camera controller
             List<PlayerKeyframe> playerKeyframes = new();
+            bool[] field = BitfieldParser(reader.ReadUInt16(), 10);
             while (reader.Position < reader.Length - 1) {
                 MessageReader message = reader.ReadMessage();
+                uint offset = message.ReadPackedUInt32();
+                uint duration = message.ReadPackedUInt32();
+                
                 playerKeyframes.Add(new PlayerKeyframe(
-                    message.ReadPackedUInt32(),
-                    message.ReadPackedUInt32(),
-                    message.ReadSingle(),
-                    message.ReadSingle(),
-                    message.ReadSingle(),
-                    message.ReadSingle(),
-                    new Color32(message.ReadByte(), message.ReadByte(), message.ReadByte(),
-                        message.ReadByte()),
-                    new Color32(message.ReadByte(), message.ReadByte(), message.ReadByte(),
-                        message.ReadByte()),
-                    new Color32(message.ReadByte(), message.ReadByte(), message.ReadByte(),
-                        message.ReadByte()),
-                    message.ReadVector2(),
-                    message.ReadVector2(),
-                    message.ReadSingle(),
+                    offset,
+                    duration,
+                    field[0] ? message.ReadSingle() : null,
+                    field[1] ? message.ReadSingle() : null,
+                    field[2] ? message.ReadSingle() : null,
+                    field[3] ? message.ReadSingle() : null,
+                    field[4] ? new Color32(message.ReadByte(), message.ReadByte(), message.ReadByte(),
+                        message.ReadByte()) : null,
+                    field[5] ? new Color32(message.ReadByte(), message.ReadByte(), message.ReadByte(),
+                        message.ReadByte()) : null,
+                    field[6] ? new Color32(message.ReadByte(), message.ReadByte(), message.ReadByte(),
+                        message.ReadByte()) : null,
+                    field[7] ? message.ReadVector2() : null,
+                    field[8] ? message.ReadVector2() : null,
+                    field[9] ? message.ReadSingle() : null,
                     Player,
                     this
                 ));
             }
 
-            this.StartCoroutine(CoPlayAnimation(playerKeyframes.ToArray(), reader.ReadBoolean()));
+            this.StartCoroutine(CoPlayAnimation(field, playerKeyframes.ToArray(), reader.ReadBoolean()));
         }
 
         [SuppressMessage("ReSharper", "AccessToModifiedClosure")]
-        private IEnumerator CoPlayAnimation(PlayerKeyframe[] frames, bool reset) {
+        [SuppressMessage("ReSharper", "PossibleInvalidOperationException")]
+        private IEnumerator CoPlayAnimation(bool[] field, PlayerKeyframe[] frames, bool reset) {
             PlayerKeyframe resetTo = SerializeCurrentState();
             PlayerKeyframe previous = resetTo;
 
@@ -73,18 +78,18 @@ namespace PolusGG.Behaviours {
 
                 yield return new WaitForSeconds(current.Offset / 1000f);
                 yield return Effects.Lerp(current.Duration / 1000f, new Action<float>(dt => {
-                    current.PlayerOpacity = Mathf.Lerp(previous.PlayerOpacity, current.PlayerOpacity, dt);
-                    current.HatOpacity = Mathf.Lerp(previous.HatOpacity, current.HatOpacity, dt);
-                    current.PetOpacity = Mathf.Lerp(previous.PetOpacity, current.PetOpacity, dt);
-                    current.SkinOpacity = Mathf.Lerp(previous.SkinOpacity, current.SkinOpacity, dt);
+                    if (field[0]) current.PlayerOpacity = Mathf.Lerp(previous.PlayerOpacity.Value, current.PlayerOpacity.Value, dt);
+                    if (field[1]) current.HatOpacity = Mathf.Lerp(previous.HatOpacity.Value, current.HatOpacity.Value, dt);
+                    if (field[2]) current.PetOpacity = Mathf.Lerp(previous.PetOpacity.Value, current.PetOpacity.Value, dt);
+                    if (field[3]) current.SkinOpacity = Mathf.Lerp(previous.SkinOpacity.Value, current.SkinOpacity.Value, dt);
                     current.SetPlayerColors(
-                        Color.Lerp(previous.MainColor, current.MainColor, dt),
-                        Color.Lerp(previous.ShadowColor, current.ShadowColor, dt),
-                        Color.Lerp(previous.VisorColor, current.VisorColor, dt)
+                        field[4] ? Color.Lerp(previous.MainColor.Value, current.MainColor.Value, dt) : null,
+                        field[5] ? Color.Lerp(previous.ShadowColor.Value, current.ShadowColor.Value, dt) : null,
+                        field[6] ? Color.Lerp(previous.VisorColor.Value, current.VisorColor.Value, dt) : null
                     );
-                    current.Scale = Vector2.Lerp(previous.Scale, current.Scale, dt);
-                    current.Position = Vector2.Lerp(previous.Position, current.Position, dt);
-                    current.Angle = Mathf.Lerp(previous.Angle, current.Angle, dt);
+                    if (field[7]) current.Scale = Vector2.Lerp(previous.Scale.Value, current.Scale.Value, dt);
+                    if (field[8]) current.Position = Vector2.Lerp(previous.Position.Value, current.Position.Value, dt);
+                    if (field[9]) current.Angle = Mathf.Lerp(previous.Angle.Value, current.Angle.Value, dt);
                 }));
 
                 previous = current;
@@ -99,6 +104,12 @@ namespace PolusGG.Behaviours {
             resetTo.Scale = resetTo.Scale;
             resetTo.Position = resetTo.Position;
             resetTo.Angle = resetTo.Angle;
+        }
+
+        public bool[] BitfieldParser(ushort value, byte size) {
+            bool[] output = new bool[size];
+            for (byte i = 0; i < size; i++) output[i] = (value & (1 << i)) != 0;
+            return output;
         }
 
         private PlayerKeyframe SerializeCurrentState() {
@@ -121,25 +132,26 @@ namespace PolusGG.Behaviours {
         }
 
         public class PlayerKeyframe {
-            private readonly float _angle;
-            public uint Duration;
-            private readonly float _hatOpacity;
-            private readonly Color32 _mainColor;
             public uint Offset;
-            private readonly float _petOpacity;
-            private readonly PlayerControl _playerControl;
+            public uint Duration;
+
+            private readonly float? _playerOpacity;
+            private readonly float? _hatOpacity;
+            private readonly float? _petOpacity;
+            private readonly float? _skinOpacity;
+            private readonly Color32? _mainColor;
+            private readonly Color32? _shadowColor;
+            private readonly Color32? _visorColor;
+            private readonly Vector2? _position;
+            private readonly Vector2? _scale;
+            private readonly float? _angle;
+
             private readonly PlayerAnimPlayer _animPlayer;
+            private readonly PlayerControl _playerControl;
 
-            private readonly float _playerOpacity;
-            private readonly Vector2 _position;
-            private readonly Vector2 _scale;
-            private readonly Color32 _shadowColor;
-            private readonly float _skinOpacity;
-            private readonly Color32 _visorColor;
-
-            public PlayerKeyframe(uint offset, uint duration, float playerOpacity, float hatOpacity, float petOpacity,
-                float skinOpacity, Color32 mainColor, Color32 shadowColor, Color32 visorColor, Vector2 scale,
-                Vector2 position, float angle, PlayerControl playerControl, PlayerAnimPlayer animPlayer) {
+            public PlayerKeyframe(uint offset, uint duration, float? playerOpacity, float? hatOpacity, float? petOpacity,
+                float? skinOpacity, Color32? mainColor, Color32? shadowColor, Color32? visorColor, Vector2? scale,
+                Vector2? position, float? angle, PlayerControl playerControl, PlayerAnimPlayer animPlayer) {
                 Offset = offset;
                 Duration = duration;
                 _playerOpacity = playerOpacity;
@@ -156,60 +168,67 @@ namespace PolusGG.Behaviours {
                 _animPlayer = animPlayer;
             }
 
-            public float PlayerOpacity {
+            public float? PlayerOpacity {
                 get => _playerOpacity;
-                set => _animPlayer._playerColor.a = value;
-            }
-
-            public float HatOpacity {
-                get => _hatOpacity;
-                set => _animPlayer._hatColor.a = value;
-            }
-
-            public float PetOpacity {
-                get => _petOpacity;
-                set => _animPlayer._petColor.a = value;
-            }
-
-            public float SkinOpacity {
-                get => _skinOpacity;
-                set => _animPlayer._skinColor.a = value;
-            }
-
-            public Color MainColor => _mainColor;
-            public Color ShadowColor => _shadowColor;
-            public Color VisorColor => _visorColor;
-
-            public Vector2 Scale {
-                get => _scale;
                 set {
+                    if (value != null) _animPlayer._playerColor.a = value.Value;
+                }
+            }
+
+            public float? HatOpacity {
+                get => _hatOpacity ?? 0;
+                set {
+                    if (value != null) _animPlayer._hatColor.a = value.Value;
+                }
+            }
+
+            public float? PetOpacity {
+                get => _petOpacity ?? 0;
+                set {
+                    if (value != null) _animPlayer._petColor.a = value.Value;
+                }
+            }
+
+            public float? SkinOpacity {
+                get => _skinOpacity ?? 0;
+                set {
+                    if (value != null) _animPlayer._skinColor.a = value.Value;
+                }
+            }
+
+            public Color? MainColor => _mainColor;
+            public Color? ShadowColor => _shadowColor;
+            public Color? VisorColor => _visorColor;
+
+            public Vector2? Scale {
+                get => _scale ?? Vector2.zero;
+                set {
+                    if (value == null) return;
                     Transform transform1 = _playerControl.transform;
-                    Vector3 ea = value;
-                    transform1.eulerAngles = ea;
+                    transform1.eulerAngles = value.Value;
                 }
             }
 
-            public Vector2 Position {
-                get => _position;
-                set {
-                    // _playerControl.myRend.;
-                }
-            }
+            //what classifies as position?
+            //how do i implement a set for this?
+            //do i move the sprite renderers? how do i do that??
+            public Vector2? Position => _position;
 
-            public float Angle {
+            public float? Angle {
                 get => _angle;
                 set {
+                    if (value == null) return;
                     Transform transform1 = _playerControl.transform;
                     Vector3 ea = transform1.eulerAngles;
-                    ea.y = value;
+                    ea.y = value.Value;
                     transform1.eulerAngles = ea;
                 }
             }
 
-            public void SetPlayerColors(Color playerColor, Color shadowColor, Color visorColor) {
-                _playerControl.myRend.material.SetColor("_BackColor", playerColor);
-                _playerControl.myRend.material.SetColor("_BodyColor", shadowColor);
-                _playerControl.myRend.material.SetColor("_VisorColor", visorColor);
+            public void SetPlayerColors(Color? playerColor, Color? shadowColor, Color? visorColor) {
+                if (playerColor.HasValue) _playerControl.myRend.material.SetColor(BackColorID, playerColor.Value);
+                if (shadowColor.HasValue) _playerControl.myRend.material.SetColor(BodyColorID, shadowColor.Value);
+                if (visorColor.HasValue) _playerControl.myRend.material.SetColor(VisorColorID, visorColor.Value);
             }
         }
     }
