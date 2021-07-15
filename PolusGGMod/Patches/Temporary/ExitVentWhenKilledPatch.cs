fa62ec9c8c5a99133d8c1f7b515535cbe6efc923
Jan Przebor <przebot@gmail.com>
2021-07-15 23:56:05 +0200

    [UT] Exit vent when getting killed

---
+++ PolusGGMod/Patches/Temporary/ExitVentWhenKilledPatch.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace PolusGG.Patches.Temporary
+{
+    [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.Die))]
+    public class ExitVentWhenKilledPatch
+    {
+        [HarmonyPostfix]
+        public static void Postfix(PlayerControl __instance) {
+            if (__instance.inVent) __instance.MyPhysics.ExitAllVents();
+        }
+    }
+}
