455b32d2f0152e0813791726aeb5bd18358bf8b5
Sanae <atmatm6000@outlook.com>
2021-07-20 03:23:44 -0600

    Player names use SetName names with GameData fallback

---
+++ PolusGGMod/Patches/Temporary/IgnoreGameDataNames.cs
@@ -0,0 +1,11 @@
+﻿using HarmonyLib;
+
+namespace PolusGG.Patches.Temporary {
+    [HarmonyPatch(typeof(GameData.PlayerInfo), nameof(GameData.PlayerInfo.PlayerName), MethodType.Getter)]
+    public class IgnoreGameDataNames {
+        public static bool PlayerName(GameData.PlayerInfo __instance, ref string __result) {
+            __result = __instance._object ? __instance._object.nameText.text : __instance._playerName;
+            return false;
+        }
+    }
+}
