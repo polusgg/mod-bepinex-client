d3d78b44775cb9bb934d0525f10a797d4bff9589
cybershard <ashwatdd01@gmail.com>
2021-07-23 00:12:32 -0400

    TMP fixes

--- PolusggSlim/Patches/Misc/TextMeshProPatch.cs
+++ PolusggSlim/Patches/Misc/TextMeshProPatch.cs
@@ -1,4 +1,6 @@
+using System.Linq;
 using HarmonyLib;
+using TMPro;
 using UnityEngine;
 
 namespace PolusggSlim.Patches.Misc
@@ -34,11 +36,11 @@ namespace PolusggSlim.Patches.Misc
         [HarmonyPatch(typeof(TextMeshProExtensions), nameof(TextMeshProExtensions.CursorPos))]
         public static class TextMeshProExtensionPatch
         {
-            public static void Postfix(ref Vector2 __result)
+            public static void Postfix(TextMeshPro self, ref Vector2 __result)
             {
                 if (__result == Vector2.zero)
                 {
-                   __result = new Vector2(0, 0.25f);
+                    __result = self.GetTextInfo(" ").lineInfo.First().lineExtents.max;
                 }
             }
         }
