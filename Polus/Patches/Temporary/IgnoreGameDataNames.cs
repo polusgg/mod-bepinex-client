936453240899ba8664b7dfc284444968aa6e4a14
Sanae <atmatm6000@outlook.com>
2021-07-23 00:54:02 -0600

    Use Object property to get GD player name from player object

--- Polus/Patches/Temporary/IgnoreGameDataNames.cs
+++ Polus/Patches/Temporary/IgnoreGameDataNames.cs
@@ -4,7 +4,7 @@ namespace Polus.Patches.Temporary {
     [HarmonyPatch(typeof(GameData.PlayerInfo), nameof(GameData.PlayerInfo.PlayerName), MethodType.Getter)]
     public class IgnoreGameDataNames {
         public static bool PlayerName(GameData.PlayerInfo __instance, ref string __result) {
-            __result = __instance._object ? __instance._object.nameText.text : __instance._playerName;
+            __result = __instance.Object ? __instance.Object.nameText.text : __instance._playerName;
             return false;
         }
     }
