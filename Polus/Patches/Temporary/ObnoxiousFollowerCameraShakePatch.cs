69e8e265f0d477b077bf8d1a649fafcb99d4697e
Sanae <atmatm6000@outlook.com>
2021-07-29 18:33:00 -0600

    Disable camera shake patch removed

--- Polus/Patches/Temporary/ObnoxiousFollowerCameraShakePatch.cs
+++ Polus/Patches/Temporary/ObnoxiousFollowerCameraShakePatch.cs
@@ -6,11 +6,11 @@ namespace Polus.Patches.Temporary {
     public class ObnoxiousFollowerCameraShakePatch {
         [HarmonyPrefix]
         public static bool Update(FollowerCamera __instance) {
-            if (__instance.Target && !__instance.Locked)
-                __instance.transform.position = Vector3.Lerp(__instance.transform.position,
-                    (Vector2) __instance.Target.transform.position + __instance.Offset, 5f * Time.deltaTime);
+            // if (__instance.Target && !__instance.Locked)
+            // __instance.transform.position = Vector3.Lerp(__instance.transform.position,
+                // (Vector2) __instance.Target.transform.position + __instance.Offset, 5f * Time.deltaTime);
 
-            return false;
+            return true;
         }
     }
 }
