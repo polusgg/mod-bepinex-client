c2d308176aafbfba4e75354d6e09f8077e275126
Jan Przebor <przebot@gmail.com>
2021-07-31 15:38:00 +0200

    Added QuickChat sanitization for TMP tags

---
+++ Polus/Patches/Temporary/QuickChatInputSanitizePatch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using HarmonyLib;
+
+namespace Polus.Patches.Temporary {
+    [HarmonyPatch(typeof(QuickChatMenu), nameof(QuickChatMenu.QuickChat), new Type[] {typeof(QuickChatMenuItem)})]
+    public class QuickChatInputSanitizePatch {
+        [HarmonyPostfix]
+        public static void Postfix(QuickChatMenu __instance) {
+            if (__instance.targetTextBox.text.Contains("<") || __instance.targetTextBox.text.Contains(">"))
+            {
+                var inTag = false;
+                var finalString = "";
+
+                foreach (char c in __instance.targetTextBox.text)
+                {
+                    if (c == '>' && inTag)
+                    {
+                        inTag = false;
+                        continue;
+                    }
+                    if (c == '<' || inTag)
+                    {
+                        inTag = true;
+                        continue;
+                    }
+                    finalString = finalString.Insert(finalString.Length, c.ToString());
+                }
+
+                __instance.targetTextBox.text = finalString;
+            }
+        }
+    }
+}
