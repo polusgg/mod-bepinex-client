7d05842245e42a4b6901f91f6294ccee5fba013a
Jan Przebor <przebot@gmail.com>
2021-07-31 13:49:46 +0200

    Fix Among Us SetName

---
+++ Polus/Patches/Temporary/FixDumbSetNamePatch.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+
+namespace Polus.Patches.Temporary {
+    [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.SetName))]
+    public class FixDumbSetNamePatch {
+        [HarmonyPrefix]
+        public static bool Prefix(PlayerControl __instance, [HarmonyArgument(0)] string name, [HarmonyArgument(1)] bool dontCensor = false) {
+            if (name == "")
+            {
+                if (GameData.Instance)
+                {
+                    GameData.Instance.UpdateName(__instance.PlayerId, name, dontCensor);
+                }
+                __instance.gameObject.name = name;
+                __instance.nameText.text = name;
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
