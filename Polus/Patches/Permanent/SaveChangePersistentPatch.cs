9deaae05a7a87597796c741dd89e261bbe0a326a
Sanae <atmatm6000@outlook.com>
2021-08-01 15:45:02 -0600

    Change save location to polus
    sad

--- Polus/Patches/Permanent/SaveChangePersistentPatch.cs
+++ Polus/Patches/Permanent/SaveChangePersistentPatch.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 namespace Polus.Patches.Permanent {
     [HarmonyPatch(typeof(PlatformPaths), nameof(PlatformPaths.persistentDataPath), MethodType.Getter)]
     public class SaveChangePersistentPatch {
-        public static string Location => Path.Combine(Application.persistentDataPath, "polous");
+        public static string Location => Path.Combine(Application.persistentDataPath, "polus");
         [PermanentPatch]
         [HarmonyPostfix]
         public static void PersistentDataPath(out string __result) {
--- Polus/Patches/Temporary/FixDumbSetNamePatch.cs
+++ Polus/Patches/Temporary/FixDumbSetNamePatch.cs
@@ -5,18 +5,14 @@ namespace Polus.Patches.Temporary {
     public class FixDumbSetNamePatch {
         [HarmonyPrefix]
         public static bool Prefix(PlayerControl __instance, [HarmonyArgument(0)] string name, [HarmonyArgument(1)] bool dontCensor = false) {
-            if (name == "")
+            if (name != "") return true;
+            if (GameData.Instance)
             {
-                if (GameData.Instance)
-                {
-                    GameData.Instance.UpdateName(__instance.PlayerId, name, dontCensor);
-                }
-                __instance.gameObject.name = name;
-                __instance.nameText.text = name;
-                return false;
+                GameData.Instance.UpdateName(__instance.PlayerId, name, dontCensor);
             }
-
-            return true;
+            __instance.gameObject.name = name;
+            __instance.nameText.text = name;
+            return false;
         }
     }
 }
