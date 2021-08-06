466f2f9d2a6558c5c6a5f2cd9e96193e6a80707f
Sanae <atmatm6000@outlook.com>
2021-08-05 20:32:22 -0600

    Try catch language loading in save manager

---
+++ Polus/Patches/Permanent/LastLanguageFixPatch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using HarmonyLib;
+
+namespace Polus.Patches.Permanent {
+    [HarmonyPatch(typeof(SaveManager), nameof(SaveManager.LastLanguage), MethodType.Getter)]
+    public static class LastLanguageFixPatch {
+        [HarmonyPrefix]
+        public static bool Awake(out uint __result) {
+            try {
+                SaveManager.LoadPlayerPrefs();
+                if (SaveManager.lastLanguage > 13) SaveManager.lastLanguage = TranslationController.SelectDefaultLanguage();
+
+                __result = SaveManager.lastLanguage;
+            } catch {
+                __result = 0;
+            }
+
+            return false;
+        }
+    }
+}
