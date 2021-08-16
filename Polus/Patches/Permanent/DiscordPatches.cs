a07983685f6870501fee3b19f682c8c78e4b9098
Rose Hall <rose@hall.ly>
2021-08-16 18:09:23 -0400

    poggin

--- Polus/Patches/Permanent/DiscordPatches.cs
+++ Polus/Patches/Permanent/DiscordPatches.cs
@@ -22,25 +22,68 @@ namespace Polus.Patches.Permanent
 
             if (reader.ReadBoolean()) {activity.State = reader.ReadString();}
             if (reader.ReadBoolean()) {activity.Details = reader.ReadString();}
-            if (reader.ReadBoolean()) {var a = activity.Timestamps; a.Start = ((long)reader.ReadUInt32() << 32) + reader.ReadUInt32();}
-            if (reader.ReadBoolean()) {var a = activity.Timestamps; a.End = ((long)reader.ReadUInt32() << 32) + reader.ReadUInt32();}
-            if (reader.ReadBoolean()) {var a = activity.Assets; a.LargeImage = reader.ReadString();}
-            if (reader.ReadBoolean()) {var a = activity.Assets; a.LargeText = reader.ReadString();}
-            if (reader.ReadBoolean()) {var a = activity.Assets; a.SmallImage = reader.ReadString();}
-            if (reader.ReadBoolean()) {var a = activity.Assets; a.SmallText = reader.ReadString();}
-            if (reader.ReadBoolean()) {var a = activity.Party; a.Id = reader.ReadString();}
-            if (reader.ReadBoolean()) {var a = activity.Party; a.Size = new PartySize();
-                var p = a.Size;
-                p.CurrentSize = reader.ReadInt32();
-                p.MaxSize = reader.ReadInt32();
+            
+            long? startTime = null;
+            long? endTime = null;
+            
+            if (reader.ReadBoolean()) {startTime = ((long)reader.ReadUInt32() << 32) + reader.ReadUInt32();}
+            if (reader.ReadBoolean()) {endTime = ((long)reader.ReadUInt32() << 32) + reader.ReadUInt32();}
+
+            var timestamps = new ActivityTimestamps();
+
+            if (startTime != null)
+            {
+                timestamps.Start = startTime.Value;
             }
-            if (reader.ReadBoolean()) {var a = activity.Secrets; a.Match = reader.ReadString();}
-            if (reader.ReadBoolean()) {var a = activity.Secrets; a.Spectate = reader.ReadString();}
-            if (reader.ReadBoolean()) {var a = activity.Secrets; a.Join = reader.ReadString();}
+
+            if (endTime != null)
+            {
+                timestamps.End = endTime.Value;
+            }
+
+            activity.Timestamps = timestamps;
+            
+            if (reader.ReadBoolean()) {activity.Assets.LargeImage = reader.ReadString();}
+            if (reader.ReadBoolean()) {activity.Assets.LargeText = reader.ReadString();}
+            if (reader.ReadBoolean()) {activity.Assets.SmallImage = reader.ReadString();}
+            if (reader.ReadBoolean()) {activity.Assets.SmallText = reader.ReadString();}
+            if (reader.ReadBoolean()) {activity.Party.Id = reader.ReadString();}
+            if (reader.ReadBoolean()) {var size = new PartySize();
+                size.CurrentSize = reader.ReadInt32();
+                size.MaxSize = reader.ReadInt32();
+                activity.Party.Size = size;
+            }
+            if (reader.ReadBoolean()) {activity.Secrets.Match = reader.ReadString();}
+            if (reader.ReadBoolean()) {activity.Secrets.Spectate = reader.ReadString();}
+            if (reader.ReadBoolean()) {activity.Secrets.Join = reader.ReadString();}
             
             discord.GetActivityManager().UpdateActivity(activity, (Action<Result>) delegate(Result r) { });
         }
         
+        [HarmonyPatch(typeof(DiscordManager), nameof(DiscordManager.SetInLobbyClient))]
+        public static class SetInLobbyClientPatch
+        {
+            [PermanentPatch]
+            [HarmonyPrefix]
+            public static bool SetInLobbyClient(DiscordManager __instance) { return false; }
+        }
+        
+        [HarmonyPatch(typeof(DiscordManager), nameof(DiscordManager.SetInLobbyHost))]
+        public static class SetInLobbyHostPatch
+        {
+            [PermanentPatch]
+            [HarmonyPrefix]
+            public static bool SetInLobbyClient(DiscordManager __instance) { return false; }
+        }
+        
+        [HarmonyPatch(typeof(DiscordManager), nameof(DiscordManager.SetPlayingGame))]
+        public static class SetPlayingGamePatch
+        {
+            [PermanentPatch]
+            [HarmonyPrefix]
+            public static bool SetInLobbyClient(DiscordManager __instance) { return false; }
+        }
+
         [HarmonyPatch(typeof(DiscordManager), nameof(DiscordManager.Start))]
         public static class DisableFixedUpdatePatch {
             [PermanentPatch]
