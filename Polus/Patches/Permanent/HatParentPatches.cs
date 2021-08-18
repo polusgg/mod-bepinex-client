d4e66ebe67151178ac41763726939194b19eb42d
Sanae <atmatm6000@outlook.com>
2021-08-18 04:52:57 -0600

    Actually make back layer work

---
+++ Polus/Patches/Permanent/HatParentPatches.cs
@@ -0,0 +1,87 @@
+﻿using HarmonyLib;
+using PowerTools;
+using UnityEngine;
+
+namespace Polus.Patches.Permanent {
+    public static class HatParentPatches {
+        [HarmonyPatch(typeof(HatParent), nameof(HatParent.LateUpdate))]
+        public static class HatLateUpdatePatch {
+            public static bool IsValid(HatParent parent, HatBehaviour bhv) {
+                return parent.FrontLayer.sprite != bhv.ClimbImage && parent.FrontLayer.sprite != bhv.FloorImage;
+            }
+
+            [HarmonyPrefix]
+            public static bool LateUpdate(HatParent __instance) {
+                // return true;
+                if (__instance.Hat != null) {
+                    HatBehaviour behaviour = __instance.Hat;
+                    Sprite front;
+                    Sprite back;
+
+                    if (__instance.Parent != null && __instance.Parent.flipX) {
+                        if (behaviour.LeftMainImage != null) {
+                            front = behaviour.LeftMainImage;
+                            __instance.FrontLayer.flipX = false;
+                        } else {
+                            front = behaviour.MainImage;
+                            __instance.FrontLayer.flipX = true;
+                        }
+
+                        if (behaviour.LeftBackImage != null) {
+                            back = behaviour.LeftBackImage;
+                            __instance.BackLayer.flipX = false;
+                        } else {
+                            back = behaviour.BackImage;
+                            __instance.BackLayer.flipX = true;
+                        }
+                    } else {
+                        if (behaviour.MainImage != null) {
+                            front = behaviour.MainImage;
+                            __instance.FrontLayer.flipX = false;
+                        } else {
+                            front = behaviour.LeftMainImage;
+                            __instance.FrontLayer.flipX = true;
+                        }
+
+                        if (behaviour.BackImage != null) {
+                            back = behaviour.BackImage;
+                            __instance.BackLayer.flipX = false;
+                        } else {
+                            back = behaviour.LeftBackImage;
+                            __instance.BackLayer.flipX = true;
+                        }
+                    }
+
+                    __instance.FrontLayer.sprite = front;
+                    __instance.BackLayer.sprite = back;
+                }
+
+                return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(HatParent), nameof(HatParent.SetIdleAnim))]
+        public static class HatParentSetIdle {
+            [HarmonyPrefix]
+            public static bool SetIdleAnim(HatParent __instance) {
+                if (!__instance.Hat)
+                    return false;
+
+                if (__instance.Hat.AltShader) {
+                    __instance.FrontLayer.sharedMaterial = __instance.Hat.AltShader;
+                    __instance.BackLayer.sharedMaterial = __instance.Hat.AltShader;
+                } else {
+                    __instance.FrontLayer.sharedMaterial = DestroyableSingleton<HatManager>.Instance.DefaultHatShader;
+                    __instance.BackLayer.sharedMaterial = DestroyableSingleton<HatManager>.Instance.DefaultHatShader;
+                }
+
+                SpriteAnimNodeSync component = __instance.GetComponent<SpriteAnimNodeSync>();
+                if (component != null) component.NodeId = __instance.Hat.NoBounce ? 1 : 0;
+
+                __instance.BackLayer.enabled = true;
+                __instance.FrontLayer.enabled = true;
+                return false;
+            }
+        }
+    }
+}
---
+++ Polus/Patches/Permanent/HatTabSeparationPatch.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HarmonyLib;
+using Polus.Extensions;
+using TMPro;
+using UnhollowerBaseLib;
+using UnityEngine;
+using static UnityEngine.Object;
+using Object = System.Object;
+
+namespace Polus.Patches.Permanent {
+    [HarmonyPatch(typeof(HatsTab), nameof(HatsTab.OnEnable))]
+    public class HatTabSeparationPatch {
+        [HarmonyPrefix]
+        public static bool OnEnable(HatsTab __instance) {
+            //TODO Separate pgg specific cosmetics by productId but keep innersloth stuff the same
+            return true;
+            Il2CppReferenceArray<HatBehaviour> allHats = DestroyableSingleton<HatManager>.Instance.GetUnlockedHats();
+            SortedList<string, List<HatBehaviour>> hatGroups = new(
+                new PaddedComparer<string>("Vanilla", "")
+            );
+
+            foreach (HatBehaviour hat in allHats) {
+                if (HatManager.Instance.GetIdFromHat(hat) < 10000000) hat.StoreName = "Vanilla";
+                if (!hatGroups.ContainsKey(hat.StoreName))
+                    hatGroups[hat.StoreName] = new List<HatBehaviour>();
+                hatGroups[hat.StoreName].Add(hat);
+            }
+
+            foreach (ColorChip instanceColorChip in __instance.ColorChips)
+                Destroy(instanceColorChip.gameObject);
+
+            __instance.ColorChips.Clear();
+            
+            TextMeshPro groupNameText = __instance.transform.parent.parent.GetComponentInChildren<GameSettingMenu>(true).GetComponentInChildren<TextMeshPro>(true);
+
+            int hatIdx = 0;
+            foreach ((string groupName, List<HatBehaviour> hats) in hatGroups) {
+                GameObject text = Instantiate(groupNameText.gameObject, __instance.scroller.Inner, true);
+                text.transform.localScale = Vector3.one;
+
+                TextMeshPro tmp = text.GetComponent<TextMeshPro>();
+                tmp.text = groupName;
+                tmp.alignment = TextAlignmentOptions.Center;
+                tmp.fontSize = 3f;
+                tmp.fontSizeMax = 3f;
+                tmp.fontSizeMin = 0f;
+
+                hatIdx = (hatIdx + 3) / 4 * 4;
+
+                float xLerp = __instance.XRange.Lerp(0.5f);
+                float yLerp = __instance.YStart - hatIdx / __instance.NumPerRow * __instance.YOffset;
+                text.transform.localPosition = new Vector3(xLerp, yLerp, -1f);
+
+                hatIdx += 4;
+                foreach (HatBehaviour hat in hats.OrderBy(HatManager.Instance.GetIdFromHat)) {
+                    float num = __instance.XRange.Lerp(hatIdx % __instance.NumPerRow / (__instance.NumPerRow - 1f));
+                    float num2 = __instance.YStart - hatIdx / __instance.NumPerRow * __instance.YOffset;
+                    ColorChip colorChip = Instantiate(__instance.ColorTabPrefab, __instance.scroller.Inner);
+                    colorChip.transform.localPosition = new Vector3(num, num2, -1f);
+                    colorChip.Button.OnClick.AddListener((Action) (() => __instance.SelectHat(hat)));
+                    colorChip.Inner.SetHat(hat, PlayerControl.LocalPlayer.Data.ColorId);
+                    colorChip.Inner.transform.localPosition = hat.ChipOffset + new Vector2(0f, -0.3f);
+                    colorChip.Tag = hat;
+                    __instance.ColorChips.Add(colorChip);
+                    hatIdx += 1;
+                }
+            }
+
+            __instance.scroller.YBounds.max = -(__instance.YStart - (hatIdx + 1) / __instance.NumPerRow * __instance.YOffset) - 3f;
+            return true;
+        }
+    }
+
+    public class PaddedComparer<T> : IComparer<T> where T : IComparable {
+        private readonly T[] forcedToBottom;
+
+        public PaddedComparer(params T[] forcedToBottom) {
+            this.forcedToBottom = forcedToBottom;
+        }
+
+        public int Compare(T x, T y) {
+            if (forcedToBottom.Contains(x) && forcedToBottom.Contains(y))
+                return StringComparer.InvariantCulture.Compare(x, y);
+
+            if (forcedToBottom.Contains(x))
+                return 1;
+            if (forcedToBottom.Contains(y))
+                return -1;
+
+            return StringComparer.InvariantCulture.Compare(x, y);
+        }
+    }
+}
