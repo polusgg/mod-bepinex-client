f35a78da7730fac2ad5f8a573ca9358d22a35010
Sanae <atmatm6000@outlook.com>
2021-08-28 14:54:20 -0600

    Probably fix "disconnected from the server" error finally

---
+++ Polus/Patches/Permanent/ConnetctionPatch.cs
@@ -0,0 +1,10 @@
+﻿using HarmonyLib;
+using InnerNet;
+
+namespace Polus.Patches.Permanent {
+    [HarmonyPatch(typeof(InnerNetClient), nameof(InnerNetClient.SendOrDisconnect))]
+    public class ConnetctionPatch {
+        [HarmonyPrefix]
+        public static bool SendOrDisconnect(InnerNetClient __instance) => __instance.AmConnected;
+    }
+}
