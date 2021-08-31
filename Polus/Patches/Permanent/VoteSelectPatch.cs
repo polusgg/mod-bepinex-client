3ce3be0a23c16a87cd38f2c5f1abd089a61b294d
Edward Smale <essmale2005@gmail.com>
2021-08-31 14:27:33 +0100

    Fix phantom being able to select players in vote menu when dead

---
+++ Polus/Patches/Permanent/VoteSelectPatch.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BepInEx.Logging;
+using HarmonyLib;
+using Il2CppSystem;
+using Polus.Behaviours;
+using Polus.Extensions;
+using UnhollowerBaseLib;
+using UnityEngine;
+using Debug = Il2CppMono.Unity.Debug;
+using Object = Il2CppSystem.Object;
+
+namespace Polus.Patches.Permanent
+{
+    [HarmonyPatch(typeof(PlayerVoteArea), nameof(PlayerVoteArea.Select))]
+    public class VoteSelectPatch
+    {
+        [HarmonyPrefix]
+        public static bool Select(PlayerVoteArea __instance)
+        {
+            if (MeetingHud.Instance.amDead)
+                return false;
+            
+            return true;
+        }
+    }
+}
