0f656ae24e09648f7ddc767243259f7ac88a2b69
Sanae <atmatm6000@outlook.com>
2021-09-08 18:54:53 -0600

    Use vanilla secure new instead of a separate one

--- Polus/Patches/Permanent/SaveManagerPatches.cs
+++ Polus/Patches/Permanent/SaveManagerPatches.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Reflection;
 using HarmonyLib;
 using InnerNet;
 using Polus.Extensions;
 using Polus.Mods.Patching;
+using UnityEngine;
 
 namespace Polus.Patches.Permanent {
     public class SaveManagerPatches {
@@ -14,6 +16,14 @@ namespace Polus.Patches.Permanent {
             public static bool Prefix(out QuickChatModes __result) {
                 __result = QuickChatModes.FreeChatOrQuickChat;
                 return false;
+            }
+        }
+
+        [HarmonyPatch(typeof(SecureDataFile), MethodType.Constructor)]
+        public static class VanillaSecureFilePatch {
+            [HarmonyPrefix]
+            public static void Constructor([HarmonyArgument(0)] out string path) {
+                path = Path.Combine(Application.persistentDataPath, "secureNew");
             }
         }
     }
